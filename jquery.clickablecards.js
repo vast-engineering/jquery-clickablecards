4fc5e3bd3ae41819b242dd1ca5d1500e3dc279a0
Branko Sekulic <branko@vast.com>
2013-01-28 21:09:51 +0100

    Replaced tabs with spaces.

--- jquery.clickablecards.js
+++ jquery.clickablecards.js
@@ -13,77 +13,77 @@
  * @param {array} exclude - array of elements within clickable area which clickableCards won't be applied on.
  * @example:
  * $('.v_resultItem').clickableCard({
- *		'defaultLink' : '.v_resultItemTitle a',
- *		'exclude' : ['.v_freeCarfax', '.v_Carfax', '.v_ab_interested', '.v_DefaultOverlayOpen'],
- *		'hoverClass' : 'hoverClassName'
- *	});
+ *      'defaultLink' : '.v_resultItemTitle a',
+ *      'exclude' : ['.v_freeCarfax', '.v_Carfax', '.v_ab_interested', '.v_DefaultOverlayOpen'],
+ *      'hoverClass' : 'hoverClassName'
+ *  });
  * @copyright vast.com
  */
 
 $.fn.clickableCards = $.fn.clickablecards = function(options) {
 
-	var i,
-		that = this,
-		defaults = {
-			'hoverClass': 'v_clickable_cards_hover',
-			'exclude': []
-		};
+    var i,
+        that = this,
+        defaults = {
+            'hoverClass': 'v_clickable_cards_hover',
+            'exclude': []
+        };
 
-	options = $.extend(defaults, options);
+    options = $.extend(defaults, options);
 
-	$(this).css('cursor', 'pointer');
+    $(this).css('cursor', 'pointer');
 
-	for (i in options.exclude) {
-		$(this).find(options.exclude[i]).on('click', function(e) {
-			$(that).data('clickable-exclude', 'click');
-		});
-	}
+    for (i in options.exclude) {
+        $(this).find(options.exclude[i]).on('click', function(e) {
+            $(that).data('clickable-exclude', 'click');
+        });
+    }
 
-	$(this).on('click', function(e) {
+    $(this).on('click', function(e) {
 
-		/** Calculating targeted link */
-		var target = e.target,
-			href,
-			selector,
-			tagName = target.tagName.toUpperCase();
-		
-		if($(this).data('clickable-exclude') == 'click'){
-			/** Handling excluded elements */
-			$(that).data('clickable-exclude', '');
-		}else if (!((tagName == 'A' || $(target).parents('a').length) && $(this).data('clickable-trigger') != 'click')) {
-			/** Don't apply clckable cards by clicking on some link inside clickable area */
-			e.preventDefault();
-			if (tagName == 'A' && $(this).data('clickable-trigger') == 'click') {
+        /** Calculating targeted link */
+        var target = e.target,
+            href,
+            selector,
+            tagName = target.tagName.toUpperCase();
+        
+        if($(this).data('clickable-exclude') == 'click'){
+            /** Handling excluded elements */
+            $(that).data('clickable-exclude', '');
+        }else if (!((tagName == 'A' || $(target).parents('a').length) && $(this).data('clickable-trigger') != 'click')) {
+            /** Don't apply clckable cards by clicking on some link inside clickable area */
+            e.preventDefault();
+            if (tagName == 'A' && $(this).data('clickable-trigger') == 'click') {
 
-				/** Click is triggered */
-				href = $(target).attr('href');
-				$(this).data('clickable-trigger', '');
+                /** Click is triggered */
+                href = $(target).attr('href');
+                $(this).data('clickable-trigger', '');
 
-				if ($(target).attr('target') == '_blank') {
-					window.open(href);
-				} else if ($(target).attr('target') == '_parent') {
-					window.parent.location = href;
-				} else {
-					window.location = href;
-				}
-			} else {
+                if ($(target).attr('target') == '_blank') {
+                    window.open(href);
+                } else if ($(target).attr('target') == '_parent') {
+                    window.parent.location = href;
+                } else {
+                    window.location = href;
+                }
+            } else {
 
-				/** User clicked somewhere on clickable area */
-				selector = options.defaultLink ? options.defaultLink : 'a:first';
-				relatedLink = $(this).find(selector);
+                /** User clicked somewhere on clickable area */
+                selector = options.defaultLink ? options.defaultLink : 'a:first';
+                relatedLink = $(this).find(selector);
 
-				$(this).data('clickable-trigger', 'click');
-				/** Trigger click on default link in order to execute attached events */
-				relatedLink.trigger('click');
-			}
-		}
-	});
+                $(this).data('clickable-trigger', 'click');
+                /** Trigger click on default link in order to execute attached events */
+                relatedLink.trigger('click');
+            }
+        }
+    });
 
-	$(this).on('mouseover', function() {
-		$(this).addClass(options.hoverClass);
-	});
+    $(this).on('mouseover', function() {
+        $(this).addClass(options.hoverClass);
+    });
 
-	$(this).on('mouseout', function() {
-		$(this).removeClass(options.hoverClass);
-	});
+    $(this).on('mouseout', function() {
+        $(this).removeClass(options.hoverClass);
+    });
 };
